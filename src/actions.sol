343b0cbbfad69cfe2f97752d817f163035e9d020
Lucas Vogelsang <l@lucasvo.com>
2020-01-24 09:28:11 +0000

    Added actions & tests for issue, lock, borrow, withdraw

--- src/actions.sol
+++ src/actions.sol
@@ -1,4 +1,11 @@
 pragma solidity ^0.5.12;
+contract NFTLike {
+    function approve(address usr, uint token) public;
+}
+
+contract TokenLike {
+    function approve(address usr, uint amount) public;
+}
 
 contract ShelfLike {
     function lock(uint loan) public;
@@ -10,9 +17,37 @@ contract ShelfLike {
     function repay(uint loan, uint wad) public;
     function shelf(uint loan) public returns(address registry,uint256 tokenId,uint price,uint principal, uint initial);
 }
+
+contract OperatorLike {
+    function supply(uint amount) public;
+    function relyInvestor(address usr) public;
+}
+
 contract Actions {
-    function issue(ShefLike shelf, address registry, uint token) public returns (uint loan) {
-        return shelf.issue(registry, token)
+    function approveNFT(NFTLike registry, address usr, uint token) public {
+        registry.approve(usr, token);
+    }
+    function approveERC20(TokenLike token, address usr, uint amount) public {
+        token.approve(usr, amount);
     }
 
+    // --- Borrower Actions ---
+    function issue(ShelfLike shelf, address registry, uint token) public returns (uint loan) {
+        return shelf.issue(registry, token);
+    }
+    function lock(ShelfLike shelf, uint loan) public {
+        shelf.lock(loan);
+    }
+    function borrow(ShelfLike shelf, uint loan, uint amount) public {
+        shelf.borrow(loan, amount);
+    }
+    function withdraw(ShelfLike shelf, uint loan, uint amount, address usr) public {
+        shelf.withdraw(loan, amount, usr);
+    }
+
+
+    // --- Lender Actions ---
+    function supply(OperatorLike operator, uint amount) public {
+        operator.supply(amount);
+    }
 }
--- src/actions.t.sol
+++ src/actions.t.sol
@@ -2,22 +2,68 @@ pragma solidity ^0.5.12;
 
 import "ds-test/test.sol";
 
-import "tinlake/core/test/system/system.sol"
+import { Proxy, ProxyRegistry } from "tinlake-proxy/proxy.sol";
+import { SystemTest } from "tinlake/core/test/system/system.sol";
+import { AdminUser } from "tinlake/core/test/system/users/admin.sol";
 import "./actions.sol";
 
 contract ActionsTest is SystemTest {
-    Actions actions;
+    address       actions;
+    address       self;
+    ProxyRegistry registry;
+
+    AdminUser public admin;
+    address admin_;
 
     function setUp() public {
-        baseSetup("whitelist", "switchable")
-        actions = new Actions();
+        baseSetup("whitelist", "switchable");
+        actions = address(new Actions());
+        registry = new ProxyRegistry();
+        self = address(this);
+
+        admin = new AdminUser(address(shelf), address(pile), address(ceiling), address(title), address(distributor));
+        admin_ = address(admin);
+        rootAdmin.relyBorrowAdmin(admin_);
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
-    }
+    function testIssueLockBorrow() public {
+        Proxy borrower    = Proxy(registry.build());
+        Proxy lender      = Proxy(registry.build());
+        address borrower_ = address(borrower);
+        address lender_   = address(lender);
 
-    function test_basic_sanity() public {
-        assertTrue(true);
+
+        // Borrower: Issue Loan, Lock NFT
+        (uint tokenId, ) = issueNFT(borrower_);
+        assertEq(collateralNFT.ownerOf(tokenId), borrower_);
+        bytes memory data = abi.encodeWithSignature("issue(address,address,uint256)", address(shelf), address(collateralNFT), tokenId);
+        bytes memory response = borrower.execute(actions, data);
+        (uint loan) = abi.decode(response, (uint));
+        assertEq(title.ownerOf(loan), borrower_);
+        borrower.execute(actions, abi.encodeWithSignature("approveNFT(address,address,uint256)", address(collateralNFT), address(shelf), loan));
+        borrower.execute(actions, abi.encodeWithSignature("lock(address,uint256)", address(shelf), loan));
+        assertEq(collateralNFT.ownerOf(1), address(shelf));
+
+        // Lend:
+        uint investment = 100 ether;
+        currency.mint(lender_, investment);
+        lender.execute(actions, abi.encodeWithSignature("approveERC20(address,address,uint256)", address(currency), address(lenderDeployer.junior()), uint(-1)));
+        address operator_ = address(lenderDeployer.juniorOperator());
+        OperatorLike(operator_).relyInvestor(lender_);
+        lender.execute(actions, abi.encodeWithSignature("supply(address,uint256)", operator_, investment));
+        // TODO: remove switchable
+        // assertEq(currency.balanceOf(address(lenderDeployer.junior())), investment);
+
+        admin.setCeiling(loan, investment);
+        // 12% per year
+        admin.doInitRate(uint(12), 1000000003593629043335673583);
+        admin.doAddRate(loan, uint(12));
+
+        // Borrow:
+        borrower.execute(actions, abi.encodeWithSignature("borrow(address,uint256,uint256)", address(shelf), loan, investment));
+        borrower.execute(actions, abi.encodeWithSignature("withdraw(address,uint256,uint256,address)", address(shelf), loan, investment, borrower_));
+
+        assertEq(currency.balanceOf(borrower_), investment);
+        assertEq(pile.debt(loan), investment);
     }
 }

b86e53754108d68c50bf154c6c35d883f3870c8d
Charly <charly@centrifuge.io>
2020-02-28 16:34:46 +0100

    fix

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,16 +1,18 @@
+BIN_DIR=${BIN_DIR:-$(cd "${0%/*}"&&pwd)}
+
 # src env for contract deployment
-source ./bin/util.sh
-source ./bin/test/local_env.sh
+source $BIN_DIR/util.sh
+source $BIN_DIR/test/local_env.sh
 
 # create address file and build contracts
-touch ./bin/test/addresses.json
+touch $BIN_DIR/test/addresses.json
 
 dapp update && dapp build --extract
 
 export ACTIONS=$(seth send --create ./out/Actions.bin 'Actions()')
 message Tinlake Actions Address: $ACTIONS
 
-DEPLOYMENT_FILE=./bin/test/addresses.json
+DEPLOYMENT_FILE=$BIN_DIR/test/addresses.json
 
 addValuesToFile $DEPLOYMENT_FILE <<EOF
 {

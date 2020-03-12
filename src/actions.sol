135b962e05137c230bc42618770f377bcff28c3c
ilinzweilin <alina@centrifuge.io>
2020-03-12 09:47:51 +0100

    feature/add-repay-full-debt

--- src/actions.sol
+++ src/actions.sol
@@ -31,7 +31,7 @@ contract ERC20Like {
 contract ShelfLike {
     function lock(uint loan) public;
     function unlock(uint loan) public;
-    function issue(address registry, uint token) public returns (uint loan);
+    function issue(address registry, uint token) public returns(uint loan);
     function close(uint loan) public;
     function borrow(uint loan, uint amount) public;
     function withdraw(uint loan, uint amount, address usr) public;
@@ -39,12 +39,16 @@ contract ShelfLike {
     function shelf(uint loan) public returns(address registry,uint256 tokenId,uint price,uint principal, uint initial);
 }
 
+contract PileLike {
+    function debt(uint loan) public returns(uint);
+}
+
 contract OperatorLike {
     function supply(uint amount) public;
     function relyInvestor(address usr) public;
 }
 
-contract Actions is DSNote {
+contract Actions is DSNote, DSTest {
     function approveNFT(NFTLike registry, address usr, uint token) public {
         registry.approve(usr, token);
     }
@@ -94,6 +98,11 @@ contract Actions is DSNote {
         shelf.repay(loan, amount);
     }
 
+    function repayFullDebt(ShelfLike shelf, PileLike pile, ERC20Like erc20, uint loan) public {
+        uint debt = pile.debt(loan);
+        repay(shelf, erc20, loan, debt);
+    }
+
     function unlock(ShelfLike shelf, NFTLike registry, uint token, uint loan) public {
         shelf.unlock(loan);
         registry.transferFrom(address(this), msg.sender, token);
@@ -103,13 +112,13 @@ contract Actions is DSNote {
         shelf.close(loan);
     }
 
-    function repayUnlock(ShelfLike shelf, NFTLike registry, uint token, ERC20Like erc20, uint loan, uint amount) public {
-        repay(shelf, erc20, loan, amount);
+    function repayUnlock(ShelfLike shelf, PileLike pile, NFTLike registry, uint token, ERC20Like erc20, uint loan) public {
+        repayFullDebt(shelf, pile, erc20, loan);
         unlock(shelf, registry, token, loan);
     }
 
-    function repayUnlockClose(ShelfLike shelf, NFTLike registry, uint token, ERC20Like erc20, uint loan, uint amount) public {
-        repayUnlock(shelf, registry, token, erc20, loan, amount);
+    function repayUnlockClose(ShelfLike shelf, PileLike pile, NFTLike registry, uint token, ERC20Like erc20, uint loan) public {
+        repayUnlock(shelf, pile, registry, token, erc20, loan);
         shelf.close(loan);
     }
 
